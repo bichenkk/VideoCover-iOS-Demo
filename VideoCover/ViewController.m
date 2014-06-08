eccc799c851b605cbec91177e05121b2247d20b5
Bi Chen Ka Kit <bichenkk@gmail.com>
2014-06-09 05:50:28 +0800

    Added Files

---
+++ VideoCover/ViewController.m
@@ -0,0 +1,95 @@
+//
+//  ViewController.m
+//  VideoCover
+//
+//  Created by Bi Chen Ka Kit on 2/6/14.
+//  Copyright (c) 2014年 biworks. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import "ViewController2.h"
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+@interface ViewController ()
+
+@property (nonatomic, strong) AVPlayer *avplayer;
+@property (strong, nonatomic) IBOutlet UIView *movieView;
+@property (strong, nonatomic) IBOutlet UIView *gradientView;
+@property (strong, nonatomic) IBOutlet UIView *contentView;
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *moviePath = [bundle pathForResource:@"xx" ofType:@"mp4"];
+    NSURL *movieURL = [NSURL fileURLWithPath:moviePath];
+    
+    AVAsset *avAsset = [AVAsset assetWithURL:movieURL];
+    AVPlayerItem *avPlayerItem =[[AVPlayerItem alloc]initWithAsset:avAsset];
+    self.avplayer = [[AVPlayer alloc]initWithPlayerItem:avPlayerItem];
+    AVPlayerLayer *avPlayerLayer =[AVPlayerLayer playerLayerWithPlayer:self.avplayer];
+    [avPlayerLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    [avPlayerLayer setFrame:self.view.frame];
+    [self.movieView.layer addSublayer:avPlayerLayer];
+    [self.avplayer seekToTime:kCMTimeZero];
+    [self.avplayer setVolume:0.0f];
+    [self.avplayer setActionAtItemEnd:AVPlayerActionAtItemEndNone];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(playerItemDidReachEnd:)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:[self.avplayer currentItem]];
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.frame = self.gradientView.bounds;
+    gradient.colors = [NSArray arrayWithObjects:(id)[UIColorFromRGB(0x030303) CGColor], (id)[[UIColor clearColor] CGColor], (id)[UIColorFromRGB(0x030303) CGColor],nil];
+    [self.gradientView.layer insertSublayer:gradient atIndex:0];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self.avplayer pause];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.avplayer play];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
+}
+
+- (void)playerItemDidReachEnd:(NSNotification *)notification {
+    AVPlayerItem *p = [notification object];
+    [p seekToTime:kCMTimeZero];
+}
+
+- (IBAction)buttonPressed:(id)sender {
+    ViewController2 *viewController = [[ViewController2 alloc]initWithNibName:@"ViewController2" bundle:nil];
+    [self.navigationController pushViewController:viewController animated:YES];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
